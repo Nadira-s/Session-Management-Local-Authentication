e84a10218ca7487a67ffb4f75ba527c9d3392421
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-01-20 15:19:31 +0500

    Initial Commit

---
+++ Session Management-Local Authentication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Session Management-Local Authentication
+//
+//  Created by Nadira Seitkazy  on 20.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Session Management-Local Authentication/Session_Management_Local_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Session_Management_Local_AuthenticationApp.swift
+//  Session Management-Local Authentication
+//
+//  Created by Nadira Seitkazy  on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Session_Management_Local_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
